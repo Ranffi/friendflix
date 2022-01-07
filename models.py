07b75aaa4a34635918bb5acc018d601634930580
Ranffi <ranffiramirez@gmail.com>
2022-01-07 22:38:44 +0000

    finished M.V.P

--- models.py
+++ models.py
@@ -6,6 +6,14 @@ watched = db.Table('watched',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('entertainment_id', db.Integer, db.ForeignKey('entertainment.id'), primary_key=True)
 )
+fav = db.Table('fav',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('entertainment_id', db.Integer, db.ForeignKey('entertainment.id'), primary_key=True)
+)
+later = db.Table('later',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('entertainment_id', db.Integer, db.ForeignKey('entertainment.id'), primary_key=True)
+)
 friends = db.Table('friends',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('friend_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
@@ -14,7 +22,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200), unique=True)
-    picture = db.Column(db.String(2000))
+    thumbnail = db.Column(db.String(2000))
+    banner = db.Column(db.String(2000))
     profile_pic= db.Column(db.String(2000))
     friends = db.relationship('User', secondary=friends, backref= 'follow', primaryjoin=id==friends.c.user_id, secondaryjoin=id==friends.c.friend_id)
 
@@ -25,12 +34,18 @@ class Entertainment(db.Model):
     year = db.Column(db.String(200))
     released = db.Column(db.String(200))
     runtime = db.Column(db.String(200))
+    rated = db.Column(db.String(200))
+    actors = db.Column(db.String(200))
+    director = db.Column(db.String(200))
+    awards = db.Column(db.String(200))
     genre = db.Column(db.String(200))
     plot = db.Column(db.String(10000))
     poster = db.Column(db.String(200))
     imdbRating = db.Column(db.String(200))
     type = db.Column(db.String(200))
     watch = db.relationship('User', secondary=watched, backref= 'shows')
+    fav = db.relationship('User', secondary=fav, backref= 'favorite')
+    later = db.relationship('User', secondary=later, backref= 'watch_later')
 
 
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, flash
 from flask.helpers import url_for
 import requests
 from flask_sqlalchemy import SQLAlchemy
@@ -7,17 +7,15 @@ from flask_login import login_user, current_user, login_required, logout_user, L
 import models
 import config 
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.app_secret
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friendflix.sqlite"
-#Optional: But it will silence the deprecation warning in the console.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return models.User.query.get(int(user_id))
@@ -26,7 +24,19 @@ def load_user(user_id):
 
 
 def seed():
-    movies = ['westworld', 'breaking bad', 'altered carbon', 'vikings', 'narcos']
+    movies = ['westworld', 'breaking bad', 'altered carbon', 'vikings', 'narcos',]
+    banners = [
+        'https://images.unsplash.com/photo-1560972550-aba3456b5564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        'https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        'https://images.unsplash.com/photo-1636277073302-eee456f208a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80',
+        'https://images.unsplash.com/photo-1494633114655-819eb91fde40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        'https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2373&q=80',
+        'https://images.unsplash.com/photo-1542977466-bbacf83cb0b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2376&q=80',
+        'https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        'https://images.unsplash.com/photo-1627716129571-05179673b885?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'
+
+    ]
+
     for i in movies:
         movie_res  = requests.get(url=f'{config.movie_api}/?apikey={config.api_key}&t={i}')
         movie_data = movie_res.json()
@@ -36,6 +46,10 @@ def seed():
             year = movie_data['Year'],
             released = movie_data['Released'],
             runtime = movie_data['Runtime'],
+            rated = movie_data['Rated'],
+            director = movie_data['Director'],
+            awards = movie_data['Awards'],
+            actors = movie_data['Actors'],
             genre = movie_data['Genre'],
             plot = movie_data['Plot'],
             poster = movie_data['Poster'],
@@ -45,40 +59,40 @@ def seed():
         db.session.add(show)
     user_res = requests.get(url='https://randomuser.me/api/?results=8')
     user_data = user_res.json()
-    
+    i = 0
     for person in user_data['results']:
         user = models.User(
             user_name = person['login']['username'],
-            picture = person['picture']['thumbnail'],
+            thumbnail = person['picture']['thumbnail'],
+            banner = banners[i],
             profile_pic = person['picture']['large'],
             password = person['login']['password']
         )
         db.session.add(user)
-
+        i += 1
     db.session.commit()
         
-
-@app.route("/all")
-def get_all_cafes():
-    # bob =db.session.query(models.User).filter_by(user_name = 'purplebear816').first()
-    # show  = db.session.query(models.Entertainment).filter_by(title = 'Vikings').first()
-    # show.shows.append(bob)
-    # db.session.add(show)
-    # db.session.commit()
-    users = db.session.query(models.User).all()
-    vikings = db.session.query(models.Entertainment).filter_by(title = 'Vikings').first()
-    for user in users:
-        user.shows.append(vikings)
-    db.session.commit()
-    return 'yup'
 
 @app.route("/")
 def hello_world():
     user =  db.session.query(models.User).filter_by(user_name = current_user.user_name).first()
     friends = user.follow
     users = db.session.query(models.User).all()
-    print(friends)
-    return render_template('index.html', d=friends, s=current_user.shows)
+    return render_template('index.html', d=friends, s=current_user.shows, favs = current_user.favorite, watchLater = current_user.watch_later)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    file= request.form.get("file", False)
+    banner= request.form.get("banner", False)
+    user =  db.session.query(models.User).filter_by(user_name = current_user.user_name).first()
+    if file:
+        user.profile_pic = file
+    if banner:
+        user.banner = banner
+    if file or banner:
+        db.session.commit()
+    return redirect(url_for('hello_world'))
 
 @app.route("/friend<int:friend_id>", methods=["GET", "POST"])
 def hello_friend(friend_id):
@@ -86,14 +100,14 @@ def hello_friend(friend_id):
     user =  db.session.query(models.User).filter_by(id = friend_id).first()
     friends = user.follow
     users = db.session.query(models.User).all()
-    print(user)
-    return render_template('friend.html', d=current_user.follow, s=user.shows, friend=user)
+    return render_template('friend.html', d=current_user.follow, s=user.shows, favs = user.favorite, watchLater = user.watch_later, friend=user)
 
 @app.route("/search/user", methods=["GET", "POST"])
 def search_user():
     name = request.form.get("username", False)
     user =  db.session.query(models.User).filter_by(user_name = name).first()
-    print(user)
+    if not user:
+        return redirect(url_for('hello_world'))
     return redirect(url_for('hello_friend', friend_id = user.id))
 
 
@@ -118,13 +132,15 @@ def unfollow(friend_id):
 def remove(movie_id, type):
     user =  db.session.query(models.User).filter_by(user_name = current_user.user_name).first()
     movie =  db.session.query(models.Entertainment).filter_by(id = movie_id).first()
-    print(user.shows)
     if type == 'current':
         user.shows.remove(movie)
         db.session.commit()
     elif type == 'favs':
-        print('favsss')
-    # db.session.commit()
+        user.favorite.remove(movie)
+        db.session.commit()
+    elif type == 'later':
+        user.watch_later.remove(movie)
+        db.session.commit()
     return redirect(url_for('hello_world'))
 
 @app.route("/single/<int:movie_id>")
@@ -134,12 +150,17 @@ def form(movie_id):
 
 @app.route('/signUp')
 def signUp():
-    return render_template('sillyForm.html')
+    return render_template('signup.html')
 
 @app.route("/sign-up", methods=["POST"])
 def receive_data():
     name = request.form["username"]
-    user = models.User(user_name=name)
+    user = models.User(
+        user_name=name, 
+        profile_pic= 'https://icon-library.com/images/default-profile-icon/default-profile-icon-24.jpg',
+        banner = 'https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        thumbnail = 'https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'
+        )
     db.session.add(user)
     db.session.commit()
     login_user(user)
@@ -168,12 +189,7 @@ def signout():
 
 @app.route("/search", methods=["POST"])
 def search():
-    # Add friends
-    # user1 = db.session.query(models.User).filter_by(user_name = 'happyladybug698').first()
-    # user2 = db.session.query(models.User).filter_by(user_name = 'angrysnake216').first()
-    # user1.follow.append(user2)
-    # print('user 1: ', user1.user_name, ' friends: ', user1.follow)
-    # db.session.commit()
+
     title = request.form["title"]
     showLookingFor = db.session.query(models.Entertainment).filter_by(title = title).first()
 
@@ -181,37 +197,42 @@ def search():
         movie_res  = requests.get(url=f'{config.movie_api}/?apikey={config.api_key}&t={title}')
         movie_data = movie_res.json()
 
-        showLookingFor = models.Entertainment(
-            title = movie_data['Title'],
-            year = movie_data['Year'],
-            released = movie_data['Released'],
-            runtime = movie_data['Runtime'],
-            genre = movie_data['Genre'],
-            plot = movie_data['Plot'],
-            poster = movie_data['Poster'],
-            imdbRating = movie_data['imdbRating'],
-            type = movie_data['Type']
-        )
-        db.session.add(showLookingFor)
-        db.session.commit()
+        if movie_data['Response'] == 'False':
+            return redirect(url_for('hello_world'))
+        else:
+            showLookingFor = models.Entertainment(
+                title = movie_data['Title'],
+                year = movie_data['Year'],
+                released = movie_data['Released'],
+                runtime = movie_data['Runtime'],
+                genre = movie_data['Genre'],
+                plot = movie_data['Plot'],
+                poster = movie_data['Poster'],
+                imdbRating = movie_data['imdbRating'],
+                type = movie_data['Type'],
+                rated = movie_data['Rated'],
+                director = movie_data['Director'],
+                awards = movie_data['Awards'],
+                actors = movie_data['Actors'],
+            )
+            db.session.add(showLookingFor)
+            db.session.commit()
     
     return render_template('singleMovie.html' , show=showLookingFor)
 
 @app.route('/watching/<int:movie_id>', methods=['POST'])
 def watching(movie_id):
     arr = request.form.getlist('current')
-    print(movie_id)
+    user =  db.session.query(models.User).filter_by(user_name = current_user.user_name).first()
+    showLookingFor = db.session.query(models.Entertainment).filter_by(id = movie_id).first()
     if 'watchList' in arr:
-        showLookingFor = db.session.query(models.Entertainment).filter_by(id = movie_id).first()
-        print(showLookingFor)
-        user =  db.session.query(models.User).filter_by(user_name = current_user.user_name).first()
         user.shows.append(showLookingFor)
-        print(current_user.shows)
+    if 'fav' in arr:
+            user.favorite.append(showLookingFor)
+    if 'rec' in arr:
+            user.watch_later.append(showLookingFor)
     db.session.commit()
     return redirect(url_for('hello_world'))
-
-#<var_name>
-#7ec00cf9 movie apikey
 
 if __name__ == '__main__':
     # seed()
